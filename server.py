6ca719bc78105b3fed8198db48dc2069c82bda12
Sipho Mabizela <smabizela3@gmail.com>
2026-06-30 13:58:13 +0200

    Implemented client connection handling by accepting incoming connections, requesting and storing usernames, notifying all users when a new client joins, confirming successful connections, and creating a dedicated thread to handle each connected client.

--- server.py
+++ server.py
@@ -34,3 +34,22 @@ def handle(client):
             usernames.remove(username)
             break
 
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+
+        client.send('USER'. encode('ascii'))
+        username = client.recv(1024).decode('ascii')
+        usernames.append(username)
+        clients.append(client)
+
+        print(f'Username of the client is {username}!')
+        broadcast(f'{username} joined the chat!' .encode('ascii'))
+        client.send('Connected to the server!' .encode('ascii'))
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
