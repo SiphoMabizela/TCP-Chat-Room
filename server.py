c82265019f13192e2c5eedf8fd5b30cbbbc91808
Sipho Mabizela <smabizela3@gmail.com>
2026-06-30 13:46:42 +0200

    Added message broadcasting and client handling functionality, enabling the server to receive and distribute messages, detect client disconnections, remove disconnected clients, and notify other users when someone leaves the chat.

--- server.py
+++ server.py
@@ -1,10 +1,36 @@
 import threading
 import socket
 
+
 host = '127.0.0.1' #localhost
 port = 55555
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
+
+
+clients = []
+usernames = []
+
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            username = usernames[index]
+            broadcast(f'{username} has left the chat' .encode('ascii'))
+            usernames.remove(username)
+            break
 
